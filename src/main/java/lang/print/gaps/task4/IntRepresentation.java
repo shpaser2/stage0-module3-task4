47fa85ca65391670e3ae20dda4879844dde93c68
Siarhei_Shpakouski <siarhei_shpakouski@epam.com>
2023-05-08 00:54:53 +0400

    all tasks done

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,14 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int c = 99;
+        int h = 104;
+        int a = 97;
+        int r = 114;
+
+        System.out.println((char)c);
+        System.out.println((char)h);
+        System.out.println((char)a);
+        System.out.println((char)r);
     }
 }
